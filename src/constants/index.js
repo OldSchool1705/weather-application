a462cd297107569b27778143df3fd746ab527c67
Artem Zubkov <oldschool1705@gmail.com>
2023-08-25 14:32:31 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = '937b1a36b0fbffc7b1123a6c8d2397de';
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+const PRESSURT_UNITS = 0.750062;
+
+export const capitalizeFirstLetter = (str) => {
+  if (!str) return "";
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export const getPressureMm = (hpa) => {
+  return Math.round(hpa * PRESSURT_UNITS);
+};
+
+export const getTime = (seconds) => {
+  return new Date(seconds * 1000).toLocaleTimeString("ru-RU", {
+    timeZone: "Atlantic/Reykjavik",
+  });
+};
